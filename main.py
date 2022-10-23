5bddd6c24d5547c9d9a36f499b51845ef9c4b5b5
Vladyslav Liebiediev <vladyslav.liebiediev@gmail.com>
2022-10-23 22:44:11 +0300

    Basic XML generation functionality

--- main.py
+++ main.py
@@ -1,42 +1,65 @@
 import xml.etree.ElementTree as ET
 
+def generateXML(val_employer_edrpou='', val_employer_name='', val_individual_rnokpp='',
+    val_individual_seria_number='', val_individual_surname='',
+    val_individual_name='', val_individual_patronymic='',
+    val_record_employer_code='', val_record_edrpo_sign='',
+    val_record_name_sign='', val_record_edrpo_lr='',
+    val_record_name_lr='', val_record_action_type='',
+    val_record_attribute_type='', val_record_action_dt='',
+    val_record_action_text='', val_record_leave_reason='',
+    val_record_doc_type='', val_record_doc_dt='', val_record_doc_number=''):
 
-document = ET.Element('DOCUMENT')
 
-employer = ET.SubElement(document, 'EMPLOYER')
+    document = ET.Element('DOCUMENT')
+    employer = ET.SubElement(document, 'EMPLOYER')
+    employer_edrpou = ET.SubElement(employer, 'EDRPOU')
+    employer_edrpou.text = val_employer_edrpou
+    employer_name = ET.SubElement(employer, 'NAME')
+    employer_name.text = val_employer_name
+    individuals = ET.SubElement(document, 'INDIVIDUALS')
+    individual = ET.SubElement(individuals, 'INDIVIDUAL')
+    individual_rnokpp = ET.SubElement(individual, 'RNOKPP')
+    individual_rnokpp.text = val_individual_rnokpp
+    individual_seria_number = ET.SubElement(individual, 'SERIA_NUMBER')
+    individual_seria_number.text = val_individual_seria_number
+    individual_surname = ET.SubElement(individual, 'SURNAME')
+    individual_surname.text = val_individual_surname
+    individual_name = ET.SubElement(individual, 'NAME')
+    individual_name.text = val_individual_name
+    individual_patronymic = ET.SubElement(individual, 'PATRONYMIC')
+    individual_patronymic.text = val_individual_patronymic
+    records = ET.SubElement(document, 'RECORDS')
+    record = ET.SubElement(records, 'RECORD')
+    record_employer_code = ET.SubElement(record, 'EMPLOYER_CODE')
+    record_employer_code.text = val_record_employer_code
+    record_edrpo_sign = ET.SubElement(record, 'EDRPO_SIGN')
+    record_edrpo_sign.text = val_record_edrpo_sign
+    record_name_sign = ET.SubElement(record, 'NAME_SIGN')
+    record_name_sign.text = val_record_name_sign
+    record_edrpo_lr = ET.SubElement(record, 'EDRPO_LR')
+    record_edrpo_lr.text = val_record_edrpo_lr
+    record_name_lr = ET.SubElement(record, 'NAME_LR')
+    record_name_lr.text = val_record_name_lr
+    record_action_type = ET.SubElement(record, 'ACTION_TYPE')
+    record_action_type.text = val_record_action_type
+    record_attribute_type = ET.SubElement(record, 'ATTRIBUTE_TYPE')
+    record_attribute_type.text = val_record_attribute_type
+    record_action_dt = ET.SubElement(record, 'ACTION_DT')
+    record_action_dt.text = val_record_action_dt
+    record_action_text = ET.SubElement(record, 'ACTION_TEXT')
+    record_action_text.text = val_record_action_text
+    record_leave_reason = ET.SubElement(record, 'LEAVE_REASON')
+    record_leave_reason.text = val_record_leave_reason
+    record_doc_type = ET.SubElement(record, 'DOC_TYPE')
+    record_doc_type.text = val_record_doc_type
+    record_doc_dt = ET.SubElement(record, 'DOC_DT')
+    record_doc_dt.text = val_record_doc_dt
+    record_doc_number = ET.SubElement(record, 'DOC_NUMBER')
+    record_doc_number.text = val_record_doc_number
 
-employer_edrpou = ET.SubElement(employer, 'EDRPOU')
-employer_name = ET.SubElement(employer, 'NAME')
-employer_edrpou.text = ''
-individuals = ET.SubElement(document, 'INDIVIDUALS')
+    tree = ET.ElementTree(document)
+    ET.indent(tree, space="\t", level=0)
+    tree.write('test.xml', xml_declaration=False, encoding='utf-8')
 
-individual = ET.SubElement(individuals, 'INDIVIDUAL')
-
-individual_rnokpp = ET.SubElement(individual, 'RNOKPP')
-individual_seria_number = ET.SubElement(individual, 'SERIA_NUMBER')
-individual_surname = ET.SubElement(individual, 'SURNAME')
-individual_name = ET.SubElement(individual, 'NAME')
-individual_patronymic = ET.SubElement(individual, 'PATRONYMIC')
-
-records = ET.SubElement(document, 'RECORDS')
-
-record = ET.SubElement(records, 'RECORD')
-
-record_employer_code = ET.SubElement(record, 'EMPLOYER_CODE')
-record_edrpo_sign = ET.SubElement(record, 'EDRPO_SIGN')
-record_name_sign = ET.SubElement(record, 'NAME_SIGN')
-record_edrpo_lr = ET.SubElement(record, 'EDRPO_LR')
-record_name_lr = ET.SubElement(record, 'NAME_LR')
-record_action_type = ET.SubElement(record, 'ACTION_TYPE')
-record_attribute_type = ET.SubElement(record, 'ATTRIBUTE_TYPE')
-record_action_dt = ET.SubElement(record, 'ACTION_DT')
-record_action_text = ET.SubElement(record, 'ACTION_TEXT')
-record_leave_reason = ET.SubElement(record, 'LEAVE_REASON')
-record_doc_type = ET.SubElement(record, 'DOC_TYPE')
-record_doc_dt = ET.SubElement(record, 'DOC_DT')
-record_doc_number = ET.SubElement(record, 'DOC_NUMBER')
-
-#write to file
-tree = ET.ElementTree(document)
-ET.indent(tree, space="\t", level=0)
-tree.write('test.xml', xml_declaration=False, encoding='utf-8')
+generateXML(val_record_doc_number='12312312')
